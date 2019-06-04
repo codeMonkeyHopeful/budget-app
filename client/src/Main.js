21ebdfc478322967c86d6e0de8ead601b2ccba61
Ryan Jasinski <jasinski.17@gmail.com>
2019-06-04 17:56:17 -0400

    checkpoint

--- client/src/Main.js
+++ client/src/Main.js
@@ -3,6 +3,12 @@ import React, { Component } from "react";
 class Main extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      userName: "",
+      loggedIn: false,
+      email: "",
+      password: ""
+    };
   }
 
   render() {
