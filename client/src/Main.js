b6db9a97b85a4002e67d09982781f0af61f6fab8
Ryan Jasinski <jasinski.17@gmail.com>
2019-06-05 09:08:44 -0400

    most routes and some react working

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,18 +1,65 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+// import NavBar from './components/NavBar.jsx';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      userName: "",
+      userId: "",
       loggedIn: false,
       email: "",
       password: ""
     };
   }
 
+  setEmail(email) {
+    this.setState({ email });
+  }
+  setPassword(password) {
+    this.setState({ password });
+  }
+
+  getUser(email, password) {
+    axios
+      .post("/users/login", { email, password })
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
   render() {
-    return <h1>Main Component</h1>;
+    return (
+      <div>
+        <input
+          type="text"
+          name="email"
+          onChange={e => {
+            this.setEmail(e.target.value);
+          }}
+        />
+        <input
+          type="text"
+          name="password"
+          onChange={e => {
+            this.setPassword(e.target.value);
+          }}
+        />
+
+        <input
+          type="submit"
+          value="Submit"
+          onClick={e => {
+            e.preventDefault();
+            this.getUser(this.state.email, this.state.password);
+          }}
+        />
+      </div>
+    );
   }
 }
 
